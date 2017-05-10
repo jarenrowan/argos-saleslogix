de1fa68a779b4e88b84951a3c4698872ef7ea447
Jason Best <jason.best@infor.com>
2017-05-10 17:07:44 -0400

    Lookup -> detail working.

--- tests/integration/pages/leads.py
+++ tests/integration/pages/leads.py
@@ -3,27 +3,45 @@ Lead list, detail, and edit pages
 """
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from .common import CommonPage
+from selenium.webdriver.common.keys import Keys
+from .common import CommonPage, wait_for_page, wait_for_animation
+import time
+
 
 class ListPage(object):
     """
     Lead list page.
     """
+
     def __init__(self, driver):
         self.common = CommonPage(driver)
         self.driver = driver
+        self.lookup_input = self.driver.find_element_by_xpath(
+            "//*[@selected='selected']//input[@name='query']")
+        self.lookup_results_xpath = "//div[@selected='selected']//div[@data-action='activateEntry']"
 
-    def navigate(self):
-        self.common.navigate()
-        self.init_elements()
+    def lookup(self, text=''):
+        self.lookup_input.click()
+        wait_for_animation()
+        self.lookup_input.clear()
+        self.lookup_input.send_keys(text)
+        self.lookup_input.send_keys(Keys.ENTER)
+        wait_for_page(self.driver, "lead_list")
 
-    def init_elements(self):
-        pass
+    def click_first_lookup_item(self):
+        results = self.driver.find_elements_by_xpath(self.lookup_results_xpath)
+        search_count = len(results)
+        if search_count > 0:
+            results[0].click()
+            wait_for_page(self.driver, "lead_detail")
+
+
 
 class DetailPage(object):
     """
     Lead detail page.
     """
+
     def __init__(self, driver):
         self.common = CommonPage(driver)
         self.driver = driver
@@ -35,10 +53,12 @@ class DetailPage(object):
     def init_elements(self):
         pass
 
+
 class EditPage(object):
     """
     Lead edit page.
     """
+
     def __init__(self, driver):
         self.common = CommonPage(driver)
         self.driver = driver
--- tests/integration/test_leads.py
+++ tests/integration/test_leads.py
@@ -8,15 +8,18 @@ from .pages.leads import ListPage, DetailPage, EditPage
 from .pages.common import ApplicationMenu, wait_for_page
 from .util import login
 
+
 class LoginTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = WebDriver(command_executor='http://localhost:4444/wd/hub',
-                                desired_capabilities={'browserName': 'chrome'})
+                                desired_capabilities={'browserName': 'chrome', 'webdriver.remote.quietExceptions': True})
         self.driver.implicitly_wait(10)
         login(self.driver, 'loup')
         self.addCleanup(self.driver.quit)
 
     def test_add_email_if_needed(self):
-        lead_list = ListPage(self.driver)
         menu = ApplicationMenu(self.driver)
         menu.open_menu_lead_list()
+        lead_list = ListPage(self.driver)
+        lead_list.lookup('Beck, John')
+        lead_list.click_first_lookup_item()
